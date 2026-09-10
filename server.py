b980db3760d28c52f5fa01c65a6f0dd6bfdfe017
TChrisVivek <chrisvivek.t@gmail.com>
2026-09-10 13:12:38 +0530

    Error handling

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI,Query, HTTPException
 from pydantic import BaseModel
 from bson import ObjectId
+from bson.errors import InvalidId
 from motor.motor_asyncio import AsyncIOMotorClient
+
 
 
 load_dotenv()
@@ -50,25 +52,40 @@ async def get_all(limit: int = 100):
 
 @app.get("/items/{item_id}")
 async def get_single_item(item_id: str):
-    item = await items_db.find_one({"_id": ObjectId(item_id)})
-
-    if item:
+    try:
+        item = await items_db.find_one({"_id": ObjectId(item_id)})
+        if not item:
+            raise HTTPException(status_code=404, detail="Item not found")
         item["_id"] = str(item["_id"])
         return item
 
-    return {"error": "Item not found"}
-
+    except InvalidId:
+        raise HTTPException(status_code=400, detail="Invalid MongoDb ID format")
 
 @app.put("/item/{item_id}")
 async def update_item(item_id: str, item:Item):
-    result = await items_db.update_one(
-        {"_id": ObjectId(item_id)},
-        {"$set": item.model_dump()}
-    )
-    return {"message": "Item Updated", "modified_count": result.modified_count}
+    try:
+        result = await items_db.update_one(
+            {"_id": ObjectId(item_id)},
+            {"$set": item.model_dump()}
+        )
+
+        if result.matched_count == 0:
+            raise HTTPException(status_code=404, detail="Item not found")
+
+        return {"message": "Item updated", "modified_count": result.modified_count}
+    except InvalidId:
+        raise HTTPException(status_code=400, detail="Invalid MondgoDB ID format")
 
 
 @app.delete("/item/{item_id}")
 async def delete_item(item_id: str):
-    result = await items_db.delete_one({"_id": ObjectId(item_id)})
-    return {"message": "Item Deleted", "deleted_count": result.deleted_count}
+    try: 
+        result = await items_db.delete_one({"_id": ObjectId(item_id)})
+
+        if result.deleted_count == 0:
+            raise HTTPException(status_code=404, detail="Item not found")
+        return {"message": "Item Deleted", "deleted_count": result.deleted_count}
+
+    except InvalidId:
+        raise HTTPException(status_code=400, detail="Invalid MongoDB ID format")
